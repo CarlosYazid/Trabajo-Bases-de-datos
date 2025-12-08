cd3065bad4b0da4235d5361258c851db29d2188f
LuisRincon16 <lurinconj@unal.edu.co>
2025-12-07 19:31:59 -0500

    Comprobación al insertar los veterinarios
    Para que no haya problemas al insertar veterinarios que ya estén creados en la tabla "usuario" (o que evite la entrada de los que no sean veterinarios)

--- cliente/cliente_insert.php
+++ cliente/cliente_insert.php
@@ -11,6 +11,35 @@ $telefono = $_POST["telefono"];
 $correo = $_POST["correo"];
 $especialidad = $_POST["especialidad"];
 
+// Verificar si la cédula ya existe en la tabla usuario
+$query0 = "SELECT cedula_ciudadania, tipo
+			FROM usuario
+			WHERE cedula_ciudadania = $cedula;";
+$result0 = mysqli_query($conn, $query0) or die(mysqli_error($conn));
+
+if($result0->num_rows > 0):
+	$fila = mysqli_fetch_array($result0);
+	if($fila['tipo'] != 'Veterinario'):
+		mysqli_close($conn);
+		exit("LA CÉDULA YA ESTÁ REGISTRADA CON OTRO TIPO DE USUARIO");
+	else:
+		$query2 = "INSERT INTO `veterinario`(`cedula_ciudadania`,`especializacion`,`tipo`) VALUES ($cedula, '$especialidad', 'Veterinario');";
+		$result2 = mysqli_query($conn, $query2) or die(mysqli_error($conn));
+
+		if($result2):
+			// Si fue exitosa, redirigirse de nuevo a la página de la entidad
+			header("Location: cliente.php");
+			mysqli_close($conn);
+			exit;
+		else:
+			echo "Ha ocurrido un error al crear la persona";
+			mysqli_close($conn);
+			exit;
+		endif;
+
+	endif;
+endif;
+
 // Query SQL a la BD. Si tienen que hacer comprobaciones, hacerlas acá (Generar una query diferente para casos especiales)
 $query1 = "INSERT INTO `usuario`(`cedula_ciudadania`,`nombre`, `apellido`, `telefono`, `correo`, `tipo`) VALUES ($cedula, '$nombre', '$apellido', $telefono, '$correo', 'Veterinario');";
 $query2 = "INSERT INTO `veterinario`(`cedula_ciudadania`,`especializacion`,`tipo`) VALUES ($cedula, '$especialidad', 'Veterinario');";
